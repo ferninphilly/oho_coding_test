bed24a2e729526e663a3cd5e8e2df6a340059c49
Fernando Pombeiro <fernpombeiro@yahoo.com>
2017-11-09 17:15:40 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+#H1 Welcome to my node.js REST API
+
+##H2 Basic Architecture:
+This project is a submission for Holiday Extras. It was working as of 9 November, 2017. 
+The project is build on AWS using three basic AWS functions: API Gateway, Lambda, and DynamoDB. 
+Here is the basic image: 
+![Lambda](https://image.slidesharecdn.com/arc308-151008042223-lva1-app6892/95/arc308-the-serverless-company-using-aws-lambda-29-638.jpg?cb=1444278232 "Lambda Architecture")
+
+So I wrote the Lambda functions in [node.js6.10](https://nodejs.org/en/blog/release/v6.1.0/). 
+There are four of them: Update, delete, get, submit (or CRUD if you prefer).
+
+The idea here is that the user hits the api gateway and the lambda function takes over and writes data sent in the POST/GET/DELETE/UPDATE body to the [DynamoDB](https://aws.amazon.com/dynamodb/) table (a nosql AWS solution). 
+
+This data will persist per the instructions- though normally DynamoDB data does not last as long.
+
+Currently I have the api gateway running here: 
+  POST - **https://msufp83hvf.execute-api.eu-west-2.amazonaws.com/prod/users**
+  GET -**https://msufp83hvf.execute-api.eu-west-2.amazonaws.com/prod/users**
+  DELETE - https://msufp83hvf.execute-api.eu-west-2.amazonaws.com/prod/users
+  GET - https://msufp83hvf.execute-api.eu-west-2.amazonaws.com/prod/users/{id}
+
+So if you would like to, for instance, submit someone you can do this: 
+```
+curl -H "Content-Type: application/json" -X POST -d '{"forename":"Fernando","surname": "Pombeiro", "email": "fernincornwall@gmailcom"}' https://msufp83hvf.execute-api.eu-west-2.amazonaws.com/prod/users
+```
+
+##Deployment
+I have utilized the [serverless architecture](https://serverless.com/) for deployment (hence you will see the serverless yaml in the project). If you want to make your own project utilizing these functions you can just run 
+```
+npm install serverless -g
+sls deploy -v aws-profile <your aws profile>
+```
+As long as you are in the same directory as my serverless.yml then you can re-create the api and all necessary lambda functions pretty easily. 
+
+The DynamoDB table here will persist data but it is also NOSQL- so you can also send more fields than just the fore/surname, date, etc. 
+
+Please let me know if you have any questions at fernincornwall@gmail.com
+
+Thanks so much for the opportunity to take on this project! 
+
+Side note: Tests, etc are all in the Lambda cloud! As this is costing me no small amount of money I will probably keep this code up until 30 November and then take it down. Thanks!
---
+++ oho_users_service/api/users.js
@@ -0,0 +1,140 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk'); 
+
+AWS.config.setPromisesDependency(require('bluebird'));
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports.submit = (event, context, callback) => {
+  const requestBody = JSON.parse(event.body);
+  const forename = requestBody.forename;
+  const surname = requestBody.surname;
+  const email = requestBody.email;
+
+  if (typeof forename !== 'string' || typeof surname !== 'string' || typeof email !== 'string') {
+    console.error('Validation Failed');
+    callback(new Error('Couldn\'t submit user because of validation errors. Did you submit all as strings?'));
+    return;
+  }
+
+  submitUserP(userInfo(forename, surname, email))
+    .then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `Sucessfully submitted user with email ${email}`,
+          userId: res.id
+        })
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      callback(null, {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: `Unable to submit user with email ${email}`, 
+          error: err
+        })
+      })
+    });
+};
+
+
+const submitUserP = user => {
+  console.log('Submitting user');
+  const userInfo = {
+    TableName: process.env.USERS_TABLE,
+    Item: user,
+  };
+  return dynamoDb.put(userInfo).promise()
+    .then(res => user);
+};
+
+const userInfo = (forename, surname, email) => {
+  const timestamp = new Date().getTime();
+  return {
+    id: uuid.v1(),
+    forename: forename,
+    surname: surname,
+    email: email,
+    submittedAt: timestamp,
+    updatedAt: timestamp,
+  };
+};
+
+module.exports.list = (event, context, callback) => {
+  var params = {
+      TableName: process.env.USERS_TABLE,
+      ProjectionExpression: "id, forename, surname, email"
+  };
+
+  console.log("Scanning Users table.");
+  const onScan = (err, data) => {
+      if (err) {
+          console.log('Scan failed to load data. Error JSON:', JSON.stringify(err, null, 2));
+          return callback(JSON.stringify({
+              error: err
+            }));
+      } else {
+          console.log("Scan succeeded.");
+          return callback(null, {
+              statusCode: 200,
+              body: JSON.stringify({
+                  users: data.Items
+              })
+          });
+      }
+
+  };
+
+  dynamoDb.scan(params, onScan);
+
+};
+
+module.exports.delete = (event, context, callback) => {
+  const params = {
+    TableName: process.env.USERS_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+dynamoDb.delete(params).promise()
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Item),
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error('Couldn\'t delete user.'));
+      return;
+    });
+};
+
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: process.env.USERS_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+dynamoDb.get(params).promise()
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Item),
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch user.'));
+      return;
+    });
+};
